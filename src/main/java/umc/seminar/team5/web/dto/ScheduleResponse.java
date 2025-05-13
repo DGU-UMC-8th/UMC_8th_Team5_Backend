45d758783a9d878a1c53962e14233a6466bc63d0
양재희 <happyjessie0318@gmail.com>
2025-05-13 21:00:36 +0900

    Feat: 중간 커밋

--- src/main/java/umc/seminar/team5/web/dto/ScheduleResponse.java
+++ src/main/java/umc/seminar/team5/web/dto/ScheduleResponse.java
@@ -5,12 +5,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 public class ScheduleResponse {
     @Getter
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CreateResult{
-        ;
+        @Getter
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class JoinResult {
+            private Long taskId;
+            private String taskName;
+            private Integer day;
+            private Integer page;
+        }
     }
 }
--- src/main/java/umc/seminar/team5/web/dto/TaskRequest.java
+++ src/main/java/umc/seminar/team5/web/dto/TaskRequest.java
@@ -1,4 +1,32 @@
 package umc.seminar.team5.web.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import umc.seminar.team5.domain.Status;
+
+import java.time.LocalDateTime;
+
 public class TaskRequest {
+    @Getter
+    public static class JoinRequest{
+        @NotBlank
+        Long Id;
+
+        @NotBlank
+        Long memberId;
+
+        @NotBlank
+        String title;
+
+        @NotBlank
+        LocalDateTime dueDate;
+
+        @NotBlank
+        String instruction;
+
+        @Enumerated(EnumType.STRING)
+        Status status;
+    }
 }
